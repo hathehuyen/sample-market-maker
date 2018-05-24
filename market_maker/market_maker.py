d57200c1322758d4d3134acca4833d38d8ece35e
hzyeyufei <hzyeyufei@corp.netease.com>
2018-05-24 15:11:52 +0800

    a

--- market_maker/market_maker.py
+++ market_maker/market_maker.py
@@ -333,15 +333,15 @@ class OrderManager:
         #balance position
 
         if position['currentQty'] > 0 and  position['currentQty'] > sell_orders[-1]['orderQty']:
-            print("cost price %f" % (cost))
-            print("current quanlity %d" % (sell_orders[-1]['orderQty']))
-            print("adjust order quanlity to %d" % (position['currentQty']))
+            # print("cost price %f" % (cost))
+            # print("current quanlity %d" % (sell_orders[-1]['orderQty']))
+            # print("adjust order quanlity to %d" % (position['currentQty']))
             sell_orders[-1]['orderQty'] = abs(position['currentQty'])
 
         if position['currentQty'] < 0 and  abs(position['currentQty']) > buy_orders[-1]['orderQty']:
-            print("cost price %f" % (cost))
-            print("current quanlity %d" % (buy_orders[-1]['orderQty']))
-            print("adjust order quanlity to %d" % (position['currentQty']))
+            # print("cost price %f" % (cost))
+            # print("current quanlity %d" % (buy_orders[-1]['orderQty']))
+            # print("adjust order quanlity to %d" % (position['currentQty']))
             buy_orders[-1]['orderQty'] = abs(position['currentQty'])
 
         return self.converge_orders(buy_orders, sell_orders)
