6b07b49f8a2738db90879319d729ac3ab6057522
hzyeyufei <hzyeyufei@corp.netease.com>
2018-05-26 14:32:33 +0800

    a

--- market_maker/market_maker.py
+++ market_maker/market_maker.py
@@ -400,9 +400,9 @@ class OrderManager:
 
     def prepare_fibonacci_order(self, index):
         if settings.EXPON_ORDER_SIZE:
-            quantity = settings.ORDER_START_SIZE
-        else:
             quantity = settings.ORDER_START_SIZE * 2 ** (index - 1)
+        else:
+            quantity = settings.ORDER_START_SIZE + ((abs(index) - 1) * settings.ORDER_STEP_SIZE)
 
         price = self.get_fibonacci_price_offset(index)
 
