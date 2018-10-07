e30da48d4a68125549dccb36e860905ec242206e
Huyen Ha <huyenht@vp9.vn>
2018-10-07 18:27:19 +0700

    monitor

--- monitor/testtg.py
+++ monitor/testtg.py
@@ -3,18 +3,25 @@ from telegram.error import NetworkError, TelegramError
 from telegram.ext import CommandHandler, Updater
 import settings
 from time import sleep
-updater = Updater(token=settings.telegram_token)
-dispatcher = updater.dispatcher
 
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+class TG(object):
+    def __init__(self, token, chat_id):
+        self.token = token
+        self.chat_id = chat_id
+        self._updater: Updater = None
 
+    def _init(self):
+        self._updater = Updater(token=settings.telegram_token)
+        self._dispatcher = self._updater.dispatcher
+        start_handler = CommandHandler('start', self.start)
+        self._dispatcher.add_handler(start_handler)
+        self._updater.start_polling()
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+    def start(self, bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-updater.start_polling()
+tg = TG(settings.telegram_token, settings.chat_id)
 
 while True:
     sleep(10)
